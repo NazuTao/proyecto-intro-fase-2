580d996094b83295c0410893ba389dce1cabd06d
NazuTao <JesusCrack23_YT@hotmail.com>
2025-03-03 08:12:17 -0400

    Cambios generales al vendedor

--- main.js
+++ main.js
@@ -11,9 +11,36 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+document.querySelectorAll(".eliminar").forEach(button => {
+    button.addEventListener("click", (e) => {
+        const confirmacion = confirm("¿Estas seguro de que quieres eliminar este producto?");
+        if (confirmacion) {
+            const row = e.target.closest("tr");
+            row.remove();
+        }
+    });
+});
+
+document.querySelectorAll(".ocultar-producto").forEach(button => {
+    button.addEventListener("click", (e) => {
+        const row = e.target.closest("tr");
+        const disponibilidadCell = row.querySelector("td:nth-child(3)");
+
+        if (button.textContent === "Ocultar") {
+            disponibilidadCell.textContent = "Oculto";
+            button.textContent = "Mostrar";
+        } else if (button.textContent === "Mostrar") {
+            disponibilidadCell.textContent = "Disponible";
+            button.textContent = "Ocultar";
+        }        
+    });
+});
+
 function publicarProducto() {
     const nombreproducto = document.querySelector('input[placeholder="Nombre del producto"]').value.trim();
     const precioproducto = document.querySelector('input[placeholder="Precio"]').value.trim();
     const categoriaproducto = document.querySelector('input[placeholder="Categoria"]').value.trim();
 }
+
+
 
