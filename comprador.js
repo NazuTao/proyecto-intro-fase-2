cf5626f6d52d029999dacfca656184205d88af4f
PiuPiu-afk <yalexmadrid07@gmail.com>
2025-03-03 20:45:43 -0400

    Catalogo y Carrito
    Botones funcionales en Catalogo, añadido el boton de poder agregar varios de una sola vez. Carrito.html ha sido creado, completamente funcional. El boton de carrito en catalogo muestra el numero de productos
    
    Cosas faltantes: Formulario de compra. Tanto el boton de Confirmar Compra en "carrito.html" como el de Comprar en "comprador.html" solo dan una alerta de compra exitosa, aunque el del carrito elimina los productos contenidos en él (esto no sucede con el del catalogo, ntp). Tienen la misma clase

---
+++ comprador.js
@@ -0,0 +1,57 @@
+function actualizarContador() {
+    const productos = JSON.parse(localStorage.getItem('productos')) || [];
+    const contador = document.getElementById('contadorProductos');
+    const totalProductos = productos.reduce((total, producto) => total + producto.cantidad, 0);
+    contador.textContent = totalProductos;
+}
+
+function agregarProducto(producto) {
+    producto.cantidad = 1;
+
+    let productos = JSON.parse(localStorage.getItem('productos')) || [];
+
+    const productoExistente = productos.find(p => p.id === producto.id);
+        if (productoExistente) {       
+            productoExistente.cantidad += 1;
+        } else {
+            productos.push(producto);
+        }
+
+    localStorage.setItem('productos', JSON.stringify(productos));
+
+    actualizarContador();
+
+    alert(`Se agregó 1 unidad de "${producto.nombre}" a la lista.`);
+}
+
+function agregarVarios(producto) {
+    const cantidad = parseInt(prompt(`¿Cuántas unidades de "${producto.nombre}" deseas agregar?`, 1));
+
+    if (isNaN(cantidad) || cantidad <= 0) {
+        alert("Cantidad no válida. Debe ser un número mayor que 0.");
+        return;
+    }
+
+    producto.cantidad = cantidad;
+
+    let productos = JSON.parse(localStorage.getItem('productos')) || [];
+
+    const productoExistente = productos.find(p => p.id === producto.id);
+
+    if (productoExistente) {
+        productoExistente.cantidad += cantidad;
+    } else {
+        productos.push(producto);
+    }
+
+    localStorage.setItem('productos', JSON.stringify(productos));
+
+    actualizarContador();
+
+    alert(`Se agregaron ${cantidad} unidades de "${producto.nombre}" a la lista.`);
+}
+
+actualizarContador();
+
+function confirmarCompra() {
+    alert("¡Compra exitosa!");}
